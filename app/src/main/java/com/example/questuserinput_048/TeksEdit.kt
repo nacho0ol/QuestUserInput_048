4c82f110cdc2cc286f479b45e5c0a9379c7c046b
nachool <nachma.olivia1394@gmail.com>
2025-10-31 15:09:36 +0700

    menambah variabel untuk menyimpan data dari komponen UI

--- app/src/main/java/com/example/questuserinput_048/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_048/TeksEdit.kt
@@ -14,4 +14,7 @@ fun FormDataDiri(modifier: Modifier
     var textAlamat by remember {mutableStateOf(value = "")}
     var textJK by remember {mutableStateOf(value = "")}
 
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jenis by remember { mutableStateOf(value = "") }
 }
