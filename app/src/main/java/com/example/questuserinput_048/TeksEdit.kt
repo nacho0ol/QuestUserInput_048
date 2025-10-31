770c27089514bc2e8fe115ac28ae80e3c68a7c4a
nachool <nachma.olivia1394@gmail.com>
2025-10-31 17:41:10 +0700

    membuat divider

--- app/src/main/java/com/example/questuserinput_048/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_048/TeksEdit.kt
@@ -99,7 +99,15 @@ fun FormDataDiri(modifier: Modifier
             }
         ){
                 Text(stringResource(R.string.submit))
-            }
+        }
+
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(R.dimen.padding_tipis),
+            color = Color.DarkGray
+        )
 
 
     }
