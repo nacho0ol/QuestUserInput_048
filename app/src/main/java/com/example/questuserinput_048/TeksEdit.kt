5d765334c39d2c05922dc3a6e802ce3ef15acc36
nachool <nachma.olivia1394@gmail.com>
2025-10-31 15:54:34 +0700

    menambahkan divider

--- app/src/main/java/com/example/questuserinput_048/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_048/TeksEdit.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -18,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -74,6 +76,15 @@ fun FormDataDiri(modifier: Modifier
                 textAlamat = it
             }
         )
+
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(id = R.dimen.divider_tipis),
+            color = Color.DarkGrey
+        )
+
 
     }
 }
