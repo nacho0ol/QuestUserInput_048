fd934d19a3842e66fb04831cbb48cc4314464731
nachool <nachma.olivia1394@gmail.com>
2025-11-01 22:54:25 +0700

    menambah class composable

--- app/src/main/java/com/example/questuserinput_048/FormRegistrasi.kt
+++ app/src/main/java/com/example/questuserinput_048/FormRegistrasi.kt
@@ -1,2 +1,10 @@
 package com.example.questuserinput_048
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun formRegist (modifier: Modifier
+){
+    textNama
+}
