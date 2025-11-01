a9e13f0af18ac8ebbde9b03d0dba2b3915279ba0
nachool <nachma.olivia1394@gmail.com>
2025-11-01 23:04:13 +0700

    menambah variabel nama dan kota

--- app/src/main/java/com/example/questuserinput_048/FormRegistrasi.kt
+++ app/src/main/java/com/example/questuserinput_048/FormRegistrasi.kt
@@ -1,10 +1,16 @@
 package com.example.questuserinput_048
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun formRegist (modifier: Modifier
 ){
-    textNama
+    var textNama by remember { mutableStateOf(value = "")}
+    var textKota by remember { mutableStateOf(value = "")}
+
 }
