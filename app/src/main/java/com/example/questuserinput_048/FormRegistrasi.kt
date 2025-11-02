d8446f003b94a0c23dfef54ce2425dd9329bbf65
nachool <nachma.olivia1394@gmail.com>
2025-11-02 12:39:18 +0700

    menambah column utama

--- app/src/main/java/com/example/questuserinput_048/FormRegistrasi.kt
+++ app/src/main/java/com/example/questuserinput_048/FormRegistrasi.kt
@@ -1,11 +1,17 @@
 package com.example.questuserinput_048
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 
 @Composable
 fun formRegist (modifier: Modifier
@@ -30,6 +36,14 @@ fun formRegist (modifier: Modifier
     var showDialog by remember { mutableStateOf(value = false) }
     var gender: List<String> = listOf("Laki-laki", "Perempuan")
 
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+    }
 
 }
