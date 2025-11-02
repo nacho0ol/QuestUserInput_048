0fb7f83e4967a6e2679abd3e4805463f11c54d44
nachool <nachma.olivia1394@gmail.com>
2025-11-02 12:40:20 +0700

    menambah text form

--- app/src/main/java/com/example/questuserinput_048/FormRegistrasi.kt
+++ app/src/main/java/com/example/questuserinput_048/FormRegistrasi.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun formRegist (modifier: Modifier
@@ -44,6 +47,11 @@ fun formRegist (modifier: Modifier
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+        Text(
+            text = stringResource(R.string.form),
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium))
+        )
     }
 
 }
